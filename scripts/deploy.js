f2168f72805fdbbd9d4dab5204d751bc47703daa
Nick1231357 <114212806+Nick1231357@users.noreply.github.com>
2023-11-21 23:53:10 +0800

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,12 +4,14 @@ async function main() {
   // Get the Points smart contract
   const Degen = await hre.ethers.getContractFactory("DegenToken");
 
+  const initialAddress = "0xC7c352C370D2a7E4D3efd66A91FAA3afE2fA8Fc2";
+
   // Deploy it
-  const degen = await Degen.deploy();
-  await degen.deployed();
+  const degen = await Degen.deploy(initialAddress);
+  await degen.waitForDeployment();
 
   // Display the contract address
-  console.log(`Degen token deployed to ${degen.address}`);
+  console.log(`Points token deployed to ${degen.target}`);
 }
 
 // Hardhat recommends this pattern to be able to use async/await everywhere
